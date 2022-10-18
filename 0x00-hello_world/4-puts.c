83dcc3cc2513cfcf2e4f21cbac70cf4b69191200
SpiryEx <ogungbenleoluwasegun@gmail.com>
2022-10-17 17:39:21 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 /**
  * main -print a line of code using put
  *
- * Return:0
+ * Return: 0
 */
 int main(void)
 {	
-	puts("\"Programming is like building a multilingual puzzle");
+ 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}		
-
+}	
 
