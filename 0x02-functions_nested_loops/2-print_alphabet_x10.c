44269edd016ada736bd520bb9c55e575823756fe
SpiryEx <ogungbenleoluwasegun@gmail.com>
2022-10-19 16:01:59 -0700

    Task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include <main.h>
 
 /**
- * print_alphabet_x10 - print 10 times the alphabet, in lowecase
-*/
-void print the_alphabet_x10
-{
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase
+ */
 
+void print_alphabet_x10(void)
+{	
 	int count = 0;
 	char letter;
 
@@ -13,6 +13,5 @@ void print the_alphabet_x10
 	{	
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-		_putchar('/n');
-	}
- 	
+			_putchar('/n');
+	}			
