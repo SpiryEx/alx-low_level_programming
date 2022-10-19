826f321040cbe8cc71721bded3f74433b88a3d46
SpiryEx <ogungbenleoluwasegun@gmail.com>
2022-10-19 15:22:00 -0700

    take 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - print 10 times the alphabet, in lowecase
 */
 void print the_alphabet_x10
-{	
+{
+
 	int count = 0;
 	char letter;
 
@@ -11,7 +13,6 @@ void print the_alphabet_x10
 	{	
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-		_putchar('\n');
-
+		_putchar('/n');
 	}
-	
+ 	
